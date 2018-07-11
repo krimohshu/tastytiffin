2ca4e917162e7d0df37497bb4ca1f1d78c7c4457
Krishan Shukla <krishan.shyam@gmail.com>
2018-07-11 11:11:29 +0100

    initial commit

---
+++ src/main/java/com/mykitchen/tastytiffin/TastytiffinApplication.java
@@ -0,0 +1,12 @@
+package com.mykitchen.tastytiffin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TastytiffinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TastytiffinApplication.class, args);
+	}
+}
